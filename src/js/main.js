d03573af6302d4ec02a486427e31a7cf942abd8d
Violetta Zarytska <zarytska.vv@gmail.com>
2021-03-18 11:48:32 +0200

    optimization a lil bit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // import listTodo from './modules/list'
-let todos; 
+let todos;
 if (localStorage.getItem("todos")) {
   todos = JSON.parse(localStorage.getItem("todos"));
 } else {
@@ -10,142 +10,24 @@ if (localStorage.getItem("todos")) {
 let count = 0;
 count = todos.length;
 
-const input = document.querySelector(".input"),
-  closeButtons = document.querySelectorAll(".delete-todo"),
-  tabsButtons = document.querySelectorAll(".button"),
+const closeButtons = document.querySelectorAll(".delete-todo"),
   todoListContainer = document.querySelector(".todo-list"),
-  tabsButtonsContainer = document.querySelector(".ltd-button-tabs"),
   tabsTodoList = document.querySelectorAll("ul");
 
 // ======= ID =======
 
 const createID = () => {
-  let id = +localStorage.getItem('idTodo');
-  return function() {
+  let id = +localStorage.getItem("idTodo");
+  return function () {
     id++;
-    localStorage.setItem('idTodo', id.toString());
+    localStorage.setItem("idTodo", id.toString());
     return id;
-  }
+  };
 };
 
 const setID = createID();
 
-
 // ======= WORKING WITH TODOS =======
-
-const createTodo = (title, completed, id, elemUl) => {
-
-  let li = document.createElement("li");
-  li.classList.add("todo-item");
-  li.setAttribute("data-id", id);
-
-  let containerDiv = document.createElement("div");
-  containerDiv.classList.add("todo-item--inner");
-
-  let div = document.createElement("div");
-  div.classList.add("todo-icon");
-
-  let span = document.createElement("span");
-  span.classList.add("todo-text", "flex-grow");
-  span.innerHTML = title;
-
-  let input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
-
-  input.checked = completed;
-  if (completed) {
-    li.classList.add("checked");
-  }
-
-  let deleteDiv = document.createElement("div");
-  let img = document.createElement('img');
-  deleteDiv.classList.add("delete-todo");
-  img.classList.add('delete-img');
-  img.setAttribute("src", "./assets/icons/close.png");
-
-  elemUl.prepend(li);
-  li.prepend(containerDiv);
-  containerDiv.append(div, span, deleteDiv);
-  div.append(input);
-  deleteDiv.append(img);
-};
-
-const checkedTodo = (event) => {
-  const input = event.target;
-  if (
-    input.tagName !== "INPUT" ||
-    input.classList.value.includes("input-edit")
-  ) {
-    return;
-  }
-
-  input.closest(".todo-item").classList.toggle("checked");
-
-  const id = input.closest(".todo-item").dataset.id;
-  todos.map((item) => {
-    if (item.id == id) {
-      item.completed = !item.completed;
-    }
-    return item;
-  });
-
-  tabsTodoList.forEach((list) => (list.innerHTML = ""));
-
-  localStorage.setItem("todos", JSON.stringify(todos));
-  listTodo(todos);
-};
-
-const countTodos = () => {
-  let countElement = document.querySelector(".ltd-button");
-
-  if (count === 1) {
-    countElement.textContent = count + " todo item";
-  } else {
-    countElement.textContent = count + " todo items";
-  }
-};
-
-const addTodo = (event) => {
-  event.preventDefault();
-
-  let inputValue = event.target.querySelector("input").value;
-  createTodo(inputValue, false, todos.length + 1, tabsTodoList[0]);
-  createTodo(inputValue, false, todos.length + 1, tabsTodoList[1]);
-  event.target.querySelector("input").value = "";
-
-  todos.push({
-    id: setID(),
-    title: inputValue,
-    completed: false,
-  });
-
-  localStorage.setItem('todos', JSON.stringify(todos));
-
-  count++;
-  countTodos();
-};
-
-const deleteTodo = (event) => {
-  if (!event.target.classList.contains("delete-img")) {
-    return;
-  }
-
-  todos = todos.filter(
-    (todo) => todo.id != event.target.closest(".todo-item").dataset.id
-  );
-
-  tabsTodoList.forEach((list) => (list.innerHTML = ""));
-  listTodo(todos);
-
-  localStorage.setItem("todos", JSON.stringify(todos));
-
-  count--;
-  if (todos.length === count) {
-    countTodos();
-  } else {
-    count++;
-  }
-};
 
 const listTodo = () => {
   //all
@@ -170,117 +52,268 @@ const listTodo = () => {
   countTodos();
 };
 
-const editTodo = (event) => {
-  const elem = event.target;
-  if (elem.tagName !== "SPAN") {
-    return;
+const createTodo = (title, completed, id, elemUl) => {
+  let li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.setAttribute("data-id", id);
+
+  let containerDiv = document.createElement("div");
+  containerDiv.classList.add("todo-item--inner");
+
+  let div = document.createElement("div");
+  div.classList.add("todo-icon");
+
+  let span = document.createElement("span");
+  span.classList.add("todo-text", "flex-grow");
+  span.innerHTML = title;
+
+  let input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+
+  input.checked = completed;
+  if (completed) {
+    li.classList.add("checked");
   }
 
-  let textarea = document.createElement("div");
-  const firsValue = elem.textContent;
+  let deleteDiv = document.createElement("div");
+  let img = document.createElement("img"),
+    pencilImg = document.createElement("img");
 
-  textarea.textContent = elem.textContent;
-  textarea.classList.add("input-edit", "todo-text-input");
-  textarea.setAttribute('contenteditable', 'true');
+  deleteDiv.classList.add("delete-todo");
+  img.classList.add("delete-img");
+  img.setAttribute("src", "./assets/icons/close.png");
 
-  elem.textContent = "";
-  elem.append(textarea);
+  pencilImg.classList.add("pencil-img");
+  pencilImg.setAttribute("src", "./assets/icons/pencil.png");
 
-  const imgArea = elem.nextSibling;
-  imgArea.firstChild.classList.replace('delete-img', 'edit-img');
-  imgArea.firstChild.setAttribute('src', './assets/icons/edit.png');
+  elemUl.prepend(li);
+  li.prepend(containerDiv);
+  containerDiv.append(div, span, deleteDiv);
+  div.append(input);
+  deleteDiv.append(pencilImg);
+  deleteDiv.append(img);
+};
 
-  const submitEditTodo = () => {
-    if (textarea.textContent !== firsValue) {
-      elem.textContent = textarea.textContent;
+const countTodos = () => {
+  let countElement = document.querySelector(".ltd-button");
 
-      todos.map((todo) => {
-        if (todo.id == elem.closest(".todo-item").dataset.id) {
-          todo.title = textarea.textContent;
-        }
-      });
+  if (count === 1) {
+    countElement.textContent = count + " todo item";
+  } else {
+    countElement.textContent = count + " todo items";
+  }
+};
 
-      tabsTodoList.forEach((todoList) => (todoList.innerHTML = ""));
-      listTodo(todos);
+// +++++++++
+const addingTodo = () => {
+  const input = document.querySelector(".input");
 
-      localStorage.setItem("todos", JSON.stringify(todos));
-    } else {
-      elem.textContent = firsValue;
-    }
+  const addTodo = (event) => {
+    event.preventDefault();
 
-    textarea.remove();
-    imgArea.firstChild.classList.replace("edit-img", "delete-img");
-    imgArea.firstChild.setAttribute("src", "./assets/icons/close.png");
+    let inputValue = event.target.querySelector("input").value;
+    createTodo(inputValue, false, todos.length + 1, tabsTodoList[0]);
+    createTodo(inputValue, false, todos.length + 1, tabsTodoList[1]);
+    event.target.querySelector("input").value = "";
 
-    // document.removeEventListener('keydown', pressEnter);
+    todos.push({
+      id: setID(),
+      title: inputValue,
+      completed: false,
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    count++;
+    countTodos();
   };
 
-  const pressEnter = (event) => {
-    if(event.code !== 'Enter') {
+  input.addEventListener("submit", addTodo);
+};
+
+// +++++++++
+const deletingTodo = () => {
+  const deleteTodo = (event) => {
+    if (!event.target.classList.contains("delete-img")) {
       return;
     }
-    console.dir(event);
-    submitEditTodo();
-  }
 
-  document.addEventListener('keyup', pressEnter);
+    todos = todos.filter(
+      (todo) => todo.id != event.target.closest(".todo-item").dataset.id
+    );
 
-  
-  imgArea.addEventListener('click' , e => {
-    if (!e.target.classList.contains("edit-img") ||
-      e.target.classList.contains("delete-img")
+    tabsTodoList.forEach((list) => (list.innerHTML = ""));
+    listTodo(todos);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    count--;
+    if (todos.length === count) {
+      countTodos();
+    } else {
+      count++;
+    }
+  };
+
+  todoListContainer.addEventListener("mousedown", deleteTodo);
+};
+
+// +++++++++
+const checkingTodo = () => {
+  const checkedTodo = (event) => {
+    const input = event.target;
+    if (
+      input.tagName !== "INPUT" ||
+      input.classList.value.includes("input-edit")
     ) {
       return;
     }
 
-    submitEditTodo();
-  });
+    input.closest(".todo-item").classList.toggle("checked");
 
-  document.body.addEventListener("mousedown", e => {
-    if (e.target.classList.contains('input-edit') ||
-        e.target.classList.contains('edit-img')) {
+    const id = input.closest(".todo-item").dataset.id;
+    todos.map((item) => {
+      if (item.id == id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+
+    tabsTodoList.forEach((list) => (list.innerHTML = ""));
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+    listTodo(todos);
+  };
+
+  todoListContainer.addEventListener("change", checkedTodo);
+};
+
+//edit img is gone WHYYYYY
+const editingTodo = () => {
+  const editTodo = (event) => {
+    const elem = event.target;
+    if (elem.tagName !== "SPAN") {
       return;
     }
 
-    elem.textContent = firsValue;
-    textarea.remove();
+    const imgArea = elem.closest('.todo-item').querySelector('.delete-img');
+    let textarea = document.createElement("div");
+    const firsValue = elem.textContent;
 
-    imgArea.firstChild.classList.replace("edit-img", "delete-img");
-    imgArea.firstChild.setAttribute("src", "./assets/icons/close.png");
-  })
-  
+    textarea.textContent = elem.textContent;
+    textarea.classList.add("input-edit", "todo-text-input");
+    textarea.setAttribute("contenteditable", "true");
+
+    elem.textContent = "";
+    elem.append(textarea);
+
+    imgArea.classList.replace("delete-img", "edit-img");
+    imgArea.setAttribute("src", "./assets/icons/edit.png");
+
+    const submitEditTodo = () => {
+      if (textarea.textContent !== firsValue) {
+        elem.textContent = textarea.textContent;
+
+        todos.map((todo) => {
+          if (todo.id == elem.closest(".todo-item").dataset.id) {
+            todo.title = textarea.textContent;
+          }
+        });
+
+        tabsTodoList.forEach((todoList) => (todoList.innerHTML = ""));
+        listTodo(todos);
+
+        localStorage.setItem("todos", JSON.stringify(todos));
+      } else {
+        elem.textContent = firsValue;
+      }
+
+      textarea.remove();
+      imgArea.classList.replace("edit-img", "delete-img");
+      imgArea.setAttribute("src", "./assets/icons/close.png");
+
+      imgArea.addEventListener("click", null);
+    };
+
+    const pressEnter = (event) => {
+      if (event.code !== "Enter") {
+        return;
+      }
+      console.dir(event);
+      submitEditTodo();
+      document.removeEventListener("keyup", pressEnter);
+    };
+
+    document.addEventListener("keyup", pressEnter);
+
+    imgArea.addEventListener("click", (e) => {
+      if (
+        !e.target.classList.contains("edit-img") ||
+        e.target.classList.contains("delete-img")
+      ) {
+        return;
+      }
+
+      submitEditTodo();
+      document.removeEventListener("keyup", pressEnter);
+    });
+
+    document.body.addEventListener("mousedown", (e) => {
+      if (
+        e.target.classList.contains("input-edit") ||
+        e.target.classList.contains("edit-img")
+      ) {
+        return;
+      }
+
+      elem.textContent = firsValue;
+      textarea.remove();
+
+      imgArea.classList.replace("edit-img", "delete-img");
+      imgArea.setAttribute("src", "./assets/icons/close.png");
+
+      document.removeEventListener("keyup", pressEnter);
+    });
+  };
+
+  todoListContainer.addEventListener("dblclick", editTodo);
 };
 
 // tabs
-const tabDone = (event) => {
-  if (event.target.tagName !== "BUTTON") {
-    return;
-  }
+// +++++++++
+const tabs = () => {
+  const tabsButtonsContainer = document.querySelector(".ltd-button-tabs"),
+    tabsButtons = document.querySelectorAll(".button");
 
-  tabsButtons.forEach((button) => {
-    button.classList.remove("button-active");
-  });
-  event.target.classList.add("button-active");
-
-  tabsTodoList.forEach((item) => (item.hidden = true));
-
-  const buttonType = event.target.dataset.buttonType;
-
-  tabsTodoList.forEach((item) => {
-    if (buttonType == item.dataset.tab) {
-      item.hidden = false;
+  const createTabs = (event) => {
+    if (event.target.tagName !== "BUTTON") {
+      return;
     }
-  });
+
+    tabsButtons.forEach((button) => {
+      button.classList.remove("button-active");
+    });
+    event.target.classList.add("button-active");
+
+    tabsTodoList.forEach((item) => (item.hidden = true));
+
+    const buttonType = event.target.dataset.buttonType;
+
+    tabsTodoList.forEach((item) => {
+      if (buttonType == item.dataset.tab) {
+        item.hidden = false;
+      }
+    });
+  };
+
+  tabsButtonsContainer.addEventListener("click", createTabs);
 };
 
 window.addEventListener("DOMContentLoaded", () => {
   listTodo();
-
-  input.addEventListener("submit", addTodo);
-  todoListContainer.addEventListener("mousedown", deleteTodo);
-  todoListContainer.addEventListener("change", checkedTodo);
-  todoListContainer.addEventListener("dblclick", editTodo);
-  tabsButtonsContainer.addEventListener("click", tabDone);
+  addingTodo();
+  deletingTodo();
+  checkingTodo();
+  editingTodo();
+  tabs();
 });
-
-
